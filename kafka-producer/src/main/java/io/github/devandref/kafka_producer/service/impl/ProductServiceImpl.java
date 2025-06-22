cf16239edfe83b3585d7ed72796351974090caf5
devandref <andredepaulacontato@gmail.com>
2025-06-22 16:39:39 -0300

    include: producer kafka

--- kafka-producer/src/main/java/io/github/devandref/kafka_producer/service/impl/ProductServiceImpl.java
+++ kafka-producer/src/main/java/io/github/devandref/kafka_producer/service/impl/ProductServiceImpl.java
@@ -1,15 +1,31 @@
 package io.github.devandref.kafka_producer.service.impl;
 
 import io.github.devandref.kafka_producer.model.CreateProductBase;
+import io.github.devandref.kafka_producer.service.ProductCreatedEvent;
 import io.github.devandref.kafka_producer.service.ProductService;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public String createProduct(CreateProductBase createProductBase) {
-        return "";
+
+        String productId = UUID.randomUUID().toString();
+        var productCreatedEvent = new ProductCreatedEvent(createProductBase.getTitle(), createProductBase.getPrice(),
+                createProductBase.getQuantity(), productId);
+
+        kafkaTemplate.send("product-created-events-topic", productCreatedEvent);
+        return productId;
     }
 
 
